b1ff5db9afe8f741fce0925082e43098d28e3c95
Zhang Shihe <shihe.zhang@compass.com>
2022-04-15 16:28:22 +0800

    test Github Action mypy

--- other/normal2.py
+++ other/normal2.py
@@ -1,9 +1,15 @@
 from typing import List  # Python 3.8 and earlier
-from faker import Faker
-import asdf
+
+
+def cookie_handler_print(cookie):
+    1 + 'x'
+
+
+def cookie_handler(cookie) -> None:
+    print(1)
+
 
 def greet_all(names: List[str]) -> None:
-    fake = Faker('it_IT')
     for name in names:
         print('Hello ' + name)
 
@@ -13,7 +19,6 @@ ages = [10, 20, 30]
 
 greet_all(names)   # Ok!
 # greet_all(ages)    # Error due to incompatible types
-
 
 def stars(*args: int, **kwargs: float) -> None:
     # 'args' has type 'tuple[int, ...]' (a tuple of ints)
