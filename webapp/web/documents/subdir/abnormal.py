df03b63df46a34df962cd05ec8fb3c06301eb11a
Zhang Shihe <shihe.zhang@compass.com>
2022-04-22 17:04:12 +0800

    test

--- webapp/web/documents/subdir/abnormal.py
+++ webapp/web/documents/subdir/abnormal.py
@@ -15,7 +15,7 @@ def greet_all(names: List[str]) -> None:
 
 
 names = ["Alice", "Bob", "Charlie"]
-ages = [20, 20, 40]
+ages = [20, 30, 40]
 
 greet_all(names)   # Ok!
 # greet_all(ages)    # Error due to incompatible types
