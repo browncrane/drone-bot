777cc1e176398e46e0c78551a55efd22f8cdb504
zhibing.chen <87839912+sobadgirl@users.noreply.github.com>
2022-05-20 17:28:52 +0800

    fix: data error

--- .github/actions/hotfix-helper.js
+++ .github/actions/hotfix-helper.js
@@ -192,11 +192,11 @@ async function autoMergePr() {
             await processFailurePr(pr)
         } else if (!existUncomplete && !existFailure) {
             console.log(`Changed files: ${pr.changed_files}`)
-            data: pr = await octokit.rest.pulls.get({
+            data: pr = (await octokit.rest.pulls.get({
                 owner,
                 repo,
                 pull_number: pr.number
-            }).data
+            })).data
             console.log(pr)
             if (pr.changed_files >= 0) {
             console.log("Ready to merge: ", pr.number);
