b6013f12f80a9cda17ac0b0804a301015b958280
zhibing.chen <87839912+sobadgirl@users.noreply.github.com>
2022-05-23 18:30:08 +0800

    fix: remove -b

--- .github/actions/hotfix-helper.js
+++ .github/actions/hotfix-helper.js
@@ -193,7 +193,7 @@ async function autoMergePr() {
                 await exec("git config user.email 'action-bot@action'")
                 await exec("git config user.name 'action-bot'")
                 const { stdout, stderr } = await exec(`\
-                    git checkout -B ${pr.base.ref} && git fetch origin ${pr.base.ref} --unshallow && git reset --h origin/${pr.base.ref} && \
+                    git checkout ${pr.base.ref} && git fetch origin ${pr.base.ref} --unshallow && git reset --h origin/${pr.base.ref} && \
                     git checkout -B ${pr.head.ref} && git fetch origin ${pr.head.ref} && git reset --h origin/${pr.head.ref} && \
                     git checkout ${pr.base.ref} && git merge --no-ff -m "Hotfix-helper: auto merge" ${pr.head.ref} && \
                     git push origin ${pr.base.ref}
