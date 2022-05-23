4182733755e73114ff30e7b7000a4863b9ee73e0
zhibing.chen <87839912+sobadgirl@users.noreply.github.com>
2022-05-23 21:27:13 +0800

    fix: status error

--- .github/actions/hotfix-helper.js
+++ .github/actions/hotfix-helper.js
@@ -199,7 +199,7 @@ async function autoMergePr() {
                     git push origin ${pr.base.ref}
                 `)
                 console.log(`out: ${stdout}`)
-                if (stderr) {
+                if (stderr && stderr.startsWith("From https://github.com")) {
                     console.log(`error: ${stderr}`)
                     await processFailurePr(pr)
                     core.setFailed(stderr)
